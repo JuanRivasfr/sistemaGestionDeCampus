df68a4b8e2a90efb01419b73be6a111336901f31
JuanRivasFr <156709972+JuanRivasfr@users.noreply.github.com>
2024-01-30 16:30:46 -0500

    feat: :sparkles:

--- modulos/camper.py
+++ modulos/camper.py
@@ -1,5 +1,17 @@
+import os
 def create():
-    print("El camper se guardo")
+    os.system('clear')
+    print("""
+++++++++++++++++++++++++++++++++++++++
++          Menu del camper           +
+++++++++++++++++++++++++++++++++++++++
+""")
+    camper={
+        "Nombre": input("Ingrese el nombre del camper: "),
+        "Apellido" : input("Ingrese el apellido del camper: "),
+        "Edad" : int(input("Ingrese la edad del camper: "))
+    }
+    print(camper)
 
 def read():
     print("Datos del camper encontrados")
@@ -16,15 +28,21 @@ def menu():
 +          Menu del camper           +
 ++++++++++++++++++++++++++++++++++++++
 """
-    print(menuu)
-    menu = ["Guardar", "Buscar", "Actualizar", "Eliminar"]
-    print("".join([f"{i+1}. {val}\n" for i, val in enumerate(menu)]))
+    menu = ["Guardar", "Buscar", "Actualizar", "Eliminar", "Salir"]
     while(True):
+        os.system('clear')
+        print(menuu)
+        print("".join([f"{i+1}. {val}\n" for i, val in enumerate(menu)]))
         try:
             opc = int(input(":"))
             if(opc<=len(menu) and opc>0):
-                print("")
-                break
+                match(opc):
+                    case 1: create()
+                    case 2: read()
+                    case 3: update()
+                    case 4: delete()
+                    case 5:
+                        break
         except ValueError:
             print("La opcion no esta habilitada")
 

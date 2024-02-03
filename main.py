4f34dfaeddf452f086949f7df1eb008720e444c3
JuanRivasFr <156709972+JuanRivasfr@users.noreply.github.com>
2024-02-02 20:11:31 -0500

    feat:

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import modulos.camper as camper
+import json
 
-camper.menu()
+
+info = {
+    "Nombre" : input("Ingrese el nombre: "),
+    "Telefono" : [
+        {
+            f"{'Fijo' if(int(input('1. Fijo 0. Celular: '))) else 'Celular'}":
+            input(f'Numero contacto {x+1}: ')
+        }
+        for x in range((int(input("Cuantos numeros de contactos tiene?: "))))
+    ]
+}
+    
+
+busqueda = input("Numero")
+for x, val in enumerate(info["Telefono"]):
+    if( busqueda in val.get("Celular")):
+        print(val)
